0549e7fb7e36ba823c4fc83c0dd1055a43f083b9
joseplss <josepls.v@hotmail.com>
2022-05-06 14:13:12 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+console.log("Starting app.js");
+console.log("Excercise 1");
+let a = 20;
+let b = 4;
+let add = a + b;
+let mins = a - b;
+let multiply = a * b;
+let dividing = a / b;
+console.log("Variable add is " + add);
+console.log("Variable minus is " + mins);
+console.log("Variable multiply is " + multiply);
+console.log("Variable dividing is " + dividing);
+//excercise 2
+console.log("Excercise 2");
+let c = 1;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Jackie";
+let lastName = "Chan";
+console.log(`c + str: ${c + str}`);
+console.log(`c + str2: ${c + str2}`);
+console.log(`c + isPresent: ${c + isPresent}`);
+console.log(`c + firstName: ${c + firstName}`);
+console.log(`c + lastName: ${c + lastName}`);
+//excericse 3
+console.log("Excercise 3");
+let d = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
+let firstName2 = "Robin";
+let lastName2 = "Williams";
+console.log(`d == str3: ${d == str3}`); //true
+console.log(`d === str3: ${d === str3}`); //false
+console.log(`!isPresent2: ${!isPresent2}`); //true
+console.log(`(eleven == str4 && d >= str3: ${"eleven" == str4 && d >= str3}`);
+console.log(`(!isPresent2 || isPresent2): ${!isPresent2 || isPresent2}`);
+console.log(`0 == false: ${0 == false}`); //true
+console.log(`0 === false: ${0 === false}`); //false
+console.log(`0 != false: ${0 != false}`);//false
+console.log(`0 !== false: ${0 !== false}`);//true
+
